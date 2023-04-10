5d59a519c1e28a4535d1cf9c185b3c9a24aeccb5
Sheena Sang <sheenasany@gmail.com>
2023-04-10 14:55:53 -0400

    added frontend login form and home redirect to login

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,26 +1,36 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router";
 import NavBar from "./NavBar.js"
 import Home from "./Home.js"
 import Login from "./Login.js"
 import SignUp from "./SignUp.js"
 // import Users from "./Users.js"
-// import Characters from "./Characters.js
+// import Characters from "./Characters.js"
 // import Templates from "./Templates.js"
 
-
 function App() {
+
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    fetch('/me')
+    .then(res => {
+      if (res.ok) {
+        res.json()
+    .then((user) => setUser(user))
+      }
+    })      
+  }, [])
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login user={user} setUser={setUser} />} />
         <Route path="/signup" element={<SignUp />} />
-        {/* <Route path="/users/*" element={<Users />} />
-        <Route path="/characters/*" element={<Characters />} />
-        <Route path="/templates/*" element={<Templates />} /> */}
+        {/* <Route path="/users/*" element={<Users />} />  */}
+        {/* <Route path="/characters/*" element={<Characters />} /> */}
+        {/* <Route path="/templates/*" element={<Templates />} /> */}
       </Routes>
     </div>
   );
