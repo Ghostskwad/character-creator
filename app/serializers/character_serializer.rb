422f56ccff9db73d1b11ac7076a7c71ea2323022
Quade Houston <quadehouston@gmail.com>
2023-06-01 12:18:50 -0400

    update serializer

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,5 +1,5 @@
 class CharacterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :history, :character_class_type
+  attributes :id, :name, :level, :history, :character_class_type
   has_one :character_class, key: :stats
 
 end
